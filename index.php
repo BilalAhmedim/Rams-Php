65deab4a0b7a5e8beb158643c1e3585d57abc572
Bilal Ahmed <bilalahmedim@gmail.com.com>
2020-10-26 20:34:58 +0530

    Adding Login in page.

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+
+# http://localhost/Login?id=1&name=bilal ---->   Home Page !!!!.
+include 'view/base.php';
+
+//if ($_SERVER['REQUEST_URI'] === '/Login') {
+//    include 'view/templates/login.html';
+//} elseif ($_SERVER['REQUEST_URI'] === '/Home' || $_SERVER['REQUEST_URI'] === '/') {
+//    include 'view/base.php';
+//}elseif($_SERVER["REQUEST_URI"] == '/Contact'){
+//    include 'view/templates/contact.html';
+//}else if($_SERVER["REQUEST_URI"] == '/About'){
+//    include 'view/templates/about.html';
+//}else{
+//    include '404.html';
+//}
---
+++ view/templates/navigation.php
@@ -0,0 +1,23 @@
+<header>
+
+    <div class="primary-nav__logo">
+        <a href="home.html">&nbsp;</a>
+    </div>
+
+    <div class="toggle">
+        <div class="toggle__bar toggle__bar--top"></div>
+        <div class="toggle__bar toggle__bar--middle"></div>
+        <div class="toggle__bar toggle__bar--bottom"></div>
+    </div>
+    <nav class="primary-nav primary-nav--float-right">
+        <ul class="primary-nav__item">
+            <li><a href='<?php echo "/Home" ?>'>Home</a></li>
+            <li><a href='<?php echo "/" ?>'>Products</a></li>
+            <li><a href='<?php echo "/About" ?>'>About</a></li>
+            <li><a href='<?php echo "/Contact" ?>'>Contact Us</a></li>
+            <li><a href='<?php echo "/Login" ?>'>Login</a></li>
+        </ul>
+    </nav>
+
+
+</header>
